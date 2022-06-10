5f62f99e8cd4a42027d68e0760e4c69d756518bf
Mario Malitesta <mario.malitesta@gmail.com>
2022-06-10 20:19:48 +0200

    API and Axios

--- src/components/DashContainer/DashContainer.js
+++ src/components/DashContainer/DashContainer.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from './DashContainer.module.scss'
 import DashItem from '../DashItem/DashItem'
+import { useState, useEffect } from 'react'
+
 
 const DashContainer = () => {
+
+  const [cases, setCases] = useState();
+
+  const fetchCases = async () =>{
+    const cases = await axios.get('https://covid-api.mmediagroup.fr/v1/cases?country=Afghanistan');
+    console.log(cases);
+  }
+
+  useEffect(()=>{fetchCases();}, [])
+
   return (
     <div className={styles['DashContainer']}>
         <DashItem />
