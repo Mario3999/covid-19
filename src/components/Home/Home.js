7de64521788dd4daf9ad7c885a7c603f3ed7a0f2
Mario3999 <mario.malitesta@gmail.com>
2022-06-23 19:04:54 +0200

    Home Component creation

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+import DashContainer from '../DashContainer/DashContainer'
+const Home = () => {
+  return (
+    <div className="App">
+      <Header />
+      <DashContainer />
+    </div>
+  )
+}
+
+export default Home
