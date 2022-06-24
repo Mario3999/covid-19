3810741891b6acd7561535f5e0a984dcb5cc3d9f
Mario3999 <mario.malitesta@gmail.com>
2022-06-24 15:36:14 +0200

    container + info-container

--- src/components/StateDetails/StateDetails.js
+++ src/components/StateDetails/StateDetails.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from './StateDetails.module.scss'
 
 const StateDetails = () => {
   return (
-    <div>StateDetails</div>
+    <div className={styles['container']}>
+      <div className={styles['info-container']}>
+        <img src="" alt="StateImg" />
+      </div>
+    </div>
   )
 }
 
