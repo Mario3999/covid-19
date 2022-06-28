35cb3e0f1853450eb26c4cfc0e550bbcd0e43ba1
Mario3999 <mario.malitesta@gmail.com>
2022-06-28 17:12:41 +0200

    unuseful previous code deletion

--- src/components/DashContainer/DashContainer.js
+++ src/components/DashContainer/DashContainer.js
@@ -12,7 +12,6 @@ const DashContainer = () => {
   const [cases, setCases] = useState();
 
   const fetchCases = async () =>{
-    // const cases = await axios.get('https://covid-api.mmediagroup.fr/v1/cases');
     const cases = await axios.get('https://api.covidtracking.com/v1/states/current.json');
     console.log(cases);
     setCases(cases);
@@ -28,15 +27,13 @@ const DashContainer = () => {
   return (
     <div className={styles['DashContainer']}>
       <GlobalContainer cases={cases}/>
+      
       {console.log(Object.entries(cases.data))}
-      {/* {Object.entries(cases.data).map(key=>{
-        return <DashItem key={key[0]} title={key[0]} deaths={key[1].All.deaths} />
-      })} */}
+      
       {Object.entries(cases.data).map(key=>(
         <Link to={`/${key[0]}`}>
           <DashItem key={key[0]} title={key[1].state} deaths={key[1].death} />
         </Link>
-        // return <DashItem key={key[0]} title={key[1].state} deaths={key[1].death} />
       ))}
     </div>
   )
