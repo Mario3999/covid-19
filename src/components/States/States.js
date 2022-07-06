7a5da68140c599b2eb6be27069810b96a69e946b
Mario3999 <mario.malitesta@gmail.com>
2022-07-06 21:57:26 +0200

    states creation

---
+++ src/components/States/States.js
@@ -0,0 +1,343 @@
+export const states = [
+    {HI: {
+      color: "#ff6666",
+      number: "13",
+      name: "Hawaii",
+      description: "Deaths: 445"
+    }},
+    {AK: {
+      number: "0",
+      color: "#ff6666",
+      name: "Alaska",
+      description: "Deaths: 305"
+    }},
+    {FL: {
+      color: "#990000",
+      number: "10",
+      name: "Florida",
+      description: "Deaths: 32266"
+    }},
+    {NH: {
+      color: "#ff6666",
+      number: "33",
+      name: "New Hampshire",
+      description: "Deaths: 1184"
+    }},
+    {VT: {
+      color: "#ff6666",
+      number: "51",
+      name: "Vermont",
+      description: "Deaths: 208"
+    }},
+    {ME: {
+      color: "#ff6666",
+      number: "23",
+      name: "Maine",
+      description: "Deaths: 706"
+    }},
+    {RI: {
+      color: "#ff3333",
+      number: "43",
+      name: "Rhode Island",
+      description: "Deaths: 2547"
+    }},
+    {NY: {
+      color: "#990000",
+      number: "37",
+      name: "New York",
+      description: "Deaths: 39029"
+    }},
+    {PA: {
+      color: "#990000",
+      number: "41",
+      name: "Pennsylvania",
+      description: "Deaths: 24349"
+    }},
+    {NJ: {
+      color: "#990000",
+      number: "34",
+      name: "New Jersey",
+      description: "Deaths: 23574"
+    }},
+    {DE: {
+      color: "#ff3333",
+      number: "9",
+      name: "Delaware",
+      description: "Deaths: 1473"
+    }},
+    {MD: {
+      color: "#cc0000",
+      number: "22",
+      name: "Maryland",
+      description: "Deaths: 7955"
+    }},
+    {VA: {
+      color: "#cc0000",
+      number: "49",
+      name: "Virginia",
+      description: "Deaths: 9596"
+    }},
+    {WV: {
+      color: "#ff3333",
+      number: "54",
+      name: "West Virginia",
+      description: "Deaths: 2325"
+    }},
+    {OH: {
+      color: "#990000",
+      number: "38",
+      name: "Ohio",
+      description: "Deaths: 17656"
+    }},
+    {IN: {
+      color: "#cc0000",
+      number: "17",
+      name: "Indiana",
+      description: "Deaths: 12737"
+    }},
+    {IL: {
+      color: "#990000",
+      number: "16",
+      name: "Illinois",
+      description: "Deaths: 23014"
+    }},
+    {CT: {
+      color: "#cc0000",
+      number: "7",
+      name: "Connecticut",
+      description: "Deaths: 7704"
+    }},
+    {WI: {
+      color: "#cc0000",
+      number: "53",
+      name: "Wisconsin",
+      description: "Deaths: 7106"
+    }},
+    {NC: {
+      color: "#cc0000",
+      number: "30",
+      name: "North Carolina",
+      description: "Deaths: 11502"
+    }},
+    {DC: {
+      color: "#ff6666",
+      number: "8",
+      name: "District of Columbia",
+      description: "Deaths: 1030"
+    }},
+    {MA: {
+      color: "#990000",
+      number: "21",
+      name: "Massachusetts",
+      description: "Deaths: 16417"
+    }},
+    {TN: {
+      color: "#cc0000",
+      number: "46",
+      name: "Tennessee",
+      description: "Deaths: 11543"
+    }},
+    {AR: {
+      color: "#ff0000",
+      number: "2",
+      name: "Arkansas",
+      description: "Deaths: 5319"
+    }},
+    {MO: {
+      color: "#cc0000",
+      number: "26",
+      name: "Missouri",
+      description: "Deaths:  8161"
+    }},
+    {GA: {
+      color: "#990000",
+      number: "11",
+      name: "Georgia",
+      description: "Deaths: 17906"
+    }},
+    {SC: {
+      color: "#cc0000",
+      number: "44",
+      name: "South Carolina",
+      description: "Deaths: 8754"
+    }},
+    {KY: {
+      color: "#ff0000",
+      number: "19",
+      name: "Kentucky",
+      description: "Deaths: 4819"
+    }},
+    {AL: {
+      color: "#cc0000",
+      number: "1",
+      name: "Alabama",
+      description: "Deaths: 10148"
+    }},
+    {LA: {
+      color: "#cc0000",
+      number: "20",
+      name: "Louisiana",
+      description: "Deaths: 9748"
+    }},
+    {MS: {
+      color: "#ff0000",
+      number: "28",
+      name: "Mississippi",
+      description: "Deaths: 6808"
+    }},
+    {IA: {
+      color: "#ff0000",
+      number: "14",
+      name: "Iowa",
+      description: "Deaths: 5558"
+    }},
+    {MN: {
+      color: "#ff0000",
+      number: "25",
+      name: "Minnesota",
+      description: "Deaths: 6550"
+    }},
+    {OK: {
+      color: "#ff0000",
+      number: "39",
+      name: "Oklahoma",
+      description: "Deaths: 4534"
+    }},
+    {TX: {
+      color: "#990000",
+      number: "47",
+      name: "Texas",
+      description: "Deaths: 44451"
+    }},
+    {NM: {
+      color: "#ff0000",
+      number: "35",
+      name: "New Mexico",
+      description: "Deaths: 3808"
+    }},
+    {KS: {
+      color: "#ff0000",
+      number: "18",
+      name: "Kansas",
+      description: "Deaths: 4812"
+    }},
+    {NE: {
+      color: "#ff3333",
+      number: "32",
+      name: "Nebraska",
+      description: "Deaths: 2113"
+    }},
+    {SD: {
+      color: "#ff3333",
+      number: "45",
+      name: "South Dakota",
+      description: "Deaths: 1900"
+    }},
+    {ND: {
+      color: "#ff3333",
+      number: "31",
+      name: "North Dakota",
+      description: "Deaths: 1478"
+    }},
+    {WY: {
+      color: "#ff6666",
+      number: "55",
+      name: "Wyoming",
+      description: "Deaths: 682"
+    }},
+    {MT: {
+      color: "#ff3333",
+      number: "29",
+      name: "Montana",
+      description: "Deaths: 1381"
+    }},
+    {CO: {
+      color: "#ff0000",
+      number: "6",
+      name: "Colorado",
+      description: "Deaths: 5989"
+    }},
+    {UT: {
+      color: "#ff3333",
+      number: "48",
+      name: "Utah",
+      description: "Deaths: 1976"
+    }},
+    {AZ: {
+      color: "#990000",
+      number: "4",
+      name: "Arizona",
+      description: "Deaths: 16328"
+    }},
+    {NV: {
+      color: "#ff0000",
+      number: "36",
+      name: "Nevada",
+      description: "Deaths: 5037"
+    }},
+    {OR: {
+      color: "#ff3333",
+      number: "40",
+      name: "Oregon",
+      description: "Deaths: 2296"
+    }},
+    {WA: {
+      color: "#ff0000",
+      number: "52",
+      name: "Washington",
+      description: "Deaths: 5041"
+    }},
+    {CA: {
+      color: "#990000",
+      number: "5",
+      name: "California",
+      description: "Deaths: 54124"
+    }},
+    {MI: {
+      color: "#990000",
+      number: "24",
+      name: "Michigan",
+      description: "Deaths: 16658"
+    }},
+    {ID: {
+      color: "#ff3333",
+      number: "15",
+      name: "Idaho",
+      description: "Deaths: 1879"
+    }},
+    {GU: {
+      color: "#ff6666",
+      number: "12",
+      hide: "yes",
+      name: "Guam",
+      description: "Deaths: 133"
+    }},
+    {VI: {
+      color: "#ff6666",
+      number: "50",
+      hide: "yes",
+      name: "Virgin Islands",
+      description: "Deaths: 25"
+    }},
+    {PR: {
+      color: "#ff3333",
+      number: "42",
+      hide: "yes",
+      name: "Puerto Rico",
+      description: "Deaths: 2062"
+    }},
+    {MP: {
+      color: "#ff6666",
+      number: "27",
+      hide: "yes",
+      name: "Northern Mariana Islands",
+      description: "Deaths: 2"
+    }},
+    {AS: {
+      color: "#ff6666",
+      number: "3",
+      hide: "yes",
+      name: "American Samoa",
+      description: "Deaths: 0"
+    }}
+]
