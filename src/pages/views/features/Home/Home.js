1ed5f963b27d75b0b1ecce243fc9136c79111644
Mario3999 <mario.malitesta@gmail.com>
2022-07-18 14:58:06 +0200

    StateDetails page responsiveness fixings

--- src/pages/views/features/Home/Home.js
+++ src/pages/views/features/Home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import DashContainer from '../../../../components/DashContainer/DashContainer'
 import Header from '../../../../components/Header/Header'
 import styles from './Home.module.scss'
 const Home = () => {
   return (
     <div className={styles['App']}>
-      <Header id={styles['header']} />
-      <div id={styles['map-container']}>
-        <div id='map'></div>
-      </div>
-      <DashContainer />
+      <Header />
+      <div id='map'></div>
     </div>
   )
 }
